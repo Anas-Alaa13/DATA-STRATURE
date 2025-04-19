361a56e1869e6844ad42ca86676805f924093f16
Abdallah-ELfaramawy <abdalallahwalid@gmail.com>
2025-04-19 23:46:17 +0200

    Create Admin.cpp

---
+++ Admin.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include<vector>
+using namespace std;
+
+void Admin::UploadCourse() {
+
+
+    string title, code, syllabus, instructorName;
+    int creditHours;
+    string choice;
+    vector<Course>courses;
+    ofstream file("Test.csv", ios::app);
+    file << "Title,Code,Syllabus,creditHourses,Instructor\n";
+    file.close();
+    do {
+
+
+        cout << "enter title of course\n";
+        cin >> title;
+        cout << "enter code of course\n";
+        cin >> code;
+        cout << "enter syllabus of course\n";
+        cin >> syllabus;
+        cout << "enter instructorName\n";
+        cin >> instructorName;
+        cout << "enter creditHours\n";
+        cin >> creditHours;
+
+        Course course(title, code, syllabus, creditHours);
+
+        courses.push_back(course);
+        ofstream file("Test.csv", ios::app);
+
+        file << title << "," << code << "," << syllabus << "," << instructorName << "," << creditHours;
+        file.close();
+
+
+        cout << "Do you want to add another course? (y/n): ";
+        cin >> choice;
+
+    } while (choice == "yes" || choice == "y");
